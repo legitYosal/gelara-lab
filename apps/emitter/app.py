7f184f36b940e9d5f872672c95f6fa8a53987cdf
Yosef Salmalian <flaslnm@gmail.com>
2023-02-15 01:49:17 +0330

    emitter completed

--- apps/emitter/app.py
+++ apps/emitter/app.py
@@ -1,6 +1,8 @@
-import mariadb
 import sys
 import os
+from time import sleep
+from random import randint
+import mariadb
 
 if __name__ == '__main__':
     try:
@@ -8,7 +10,7 @@ if __name__ == '__main__':
             user=os.environ.get('MARIADB_USER'),
             password=os.environ.get('MARIADB_PASSWORD'),
             host=os.environ.get('MARIADB_HOST'),
-            port=os.environ.get('MARIADB_PORT'),
+            port=int(os.environ.get('MARIADB_PORT')),
             database=os.environ.get('MARIADB_DATABASE')
         )
     except mariadb.Error as e:
@@ -16,15 +18,20 @@ if __name__ == '__main__':
         sys.exit(1)
 
     cur = conn.cursor()
-    table = 'test_table'
 
-    if table not in cur.execute('show tables'):
+    cur.execute('show tables')
+    if 'test_table' not in [item[0] for item in cur.fetchall()]:
         cur.execute('create table test_table (name VARCHAR(255))')
-        cur.commit()
-        print('created table: ', table)
+        print('created table: test_table')
 
-    cur.execute(f'insert into {table} (name) VALUES (?, ?)', ('test')) 
-    cur.commit()
-    print(f'Inserted ID: {cur.lastrowid}')
+    sleep(1)
+    rand1 = randint(1, 100)
+    for i in range(randint(100, 10000)):
+        sleep(0.01 + (rand1 / 100)/randint(1,10))
+        cur.execute(f'insert into test_table (name) VALUES (?)', ['test' + str(i)]) 
+        print(f'Inserted Name: ', 'test' + str(i))
+        if randint(1, 100) > 70:
+            break
+    sleep(10)
 
     conn.close()
